70025526c390343b5c9ba6747d638a296b4d2ef2
Garrett Martin <garrett@standalone.co>
2016-01-21 12:22:04 -0500

    Kweb Launch Scripts
    Instead of the pi booting directly into the kiosk mode, this allows you
    to launch different versions of kiosk.

---
+++ Launcher Scripts/Jerrie_Mock_Kiosk_FLIGHT_MAP.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kweb3 -JLKHCA+-zbhrqfpoklgtjneduwxya http://www.arcgis.com/apps/MapJournal/index.html?appid=a789f2920df14f95b05552cf77280f22
---
+++ Launcher Scripts/Jerrie_Mock_Kiosk_MEET_JERRIE.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kweb3 -JLKHCA+-zbhrqfpoklgtjneduwxya file:///var/www/SAMHdevKiosk/index.html
